831c3dac2883cc4f0c2e8e48f5218e4ac1e1244e
wiscdecathlon <nathanpatrickbrown@gmail.com>
2017-05-26 14:01:56 +0200

    Add files via upload

---
+++ app.R
@@ -0,0 +1,353 @@
+
+#required libraries
+library(shiny)
+library(rpart)
+library(rpart.plot)
+library(caret)
+library(stats)
+library(e1071)
+library(plyr)
+
+# Define UI for application 
+ ui <- fluidPage(
+   div(class = "header",
+       includeHTML("header.html")
+   ),
+  # Application title
+ # headerPanel("Is an Elective Cath Needed?"),
+  
+  
+   # Sidebar entry area
+   sidebarLayout(
+      sidebarPanel(
+        htmlOutput("prediction",container = tags$div,class="well", style="background:#EFEFEF;width:100%;"),
+        radioButtons("sex","Gender:",
+                     choices = list("Male" = "Male", "Female" = "Female"), 
+                     selected = "Male"),
+        sliderInput("age", "Age:", min = 20,  max = 110, value = 50),
+        selectInput('cp', "Symptoms:", c(Choose='', c("Exertional (cp-1)"="1", "Atypical (cp-2)"="2","Momentary (cp-3)"="3","Absence of Pain"="None")), selectize=TRUE),
+        sliderInput("trestbps", "Systolic Blood Pressure:", min = 70,  max = 250, value = 110),
+        sliderInput("chol", "Total Cholesterol:", min = 120,  max = 400, value = 180),
+        selectInput('fbs', "Blood Sugar:", c(Choose='', c("Normal"="Normal", "Diabetic Range"="High")), selectize=TRUE),
+        selectInput('restecg', "Rest EKG Features:", c(Choose='', c("Normal"="Normal", "ST segment depression"="ST segment","Left Ventricular Hypertrophy"="LVH")), selectize=TRUE)
+       ),
+     
+      mainPanel(
+        
+        tabsetPanel(
+          tabPanel("About", 
+                   h3("What Is It?"),
+                   p("This app is a risk calculator that uses Machine Learning to determine the likelihood that coronary stenosis will actually be found in a candidate patient for elective angiography."),
+                   h3("How Is It Used?"),
+                   p("Enter patient pre-test attributes (de-identified) at left to calculate a risk prediction. Customize the data SOURCE(s) if desired, view the MODEL to better understand prediction decisions, and observe the dependence of model VALIDITY on data sources."),
+                   h3("How Does It Work?"),
+                   p("Machine Learning refers to techniques of computation used to predict future events 
+                  from analysis of past data. In this app, a prediction MODEL is created from pre-test data 
+                  SOURCE(s), and is applied to a new patient (at left), to compute an expected probability of having a positive cardiac catheterization. The source data for the model is configurable."),
+                   h3("To Whom Does It Apply?"),
+                   p("The source data sets were collected from new cardiology patients, with no prior (known) coronary artery disease. Source data is included from 4 geographic areas.
+                  Each source data set contains patients scheduled for an elective cardiac catheterization, and with NO previous non-invasive (i.e. stress) test. The model 
+                  predicts the probability of whether a cath candidate will or will not be found to have coronary disease of >50% stenosis on the cath. 
+                  Low-probability results may indicate benefit to choosing a plan of non-invasive testing and medical management instead, coupled with continued primary prevention."),
+                   h3("To Whom Does It NOT Apply?"),
+                   p("Due to inclusion criteria of the source data, this does not generalize to the general public or lower risk patients who are not thought to be candidates for angiography. Furthermore, it is not applicable to the setting of acute myocardial infarction.")),
+                   #tableOutput("values"),
+                   
+                  
+          tabPanel("Source",
+                   h2("Select Source:"),                  
+                   # Show a plot of the individual within the training data
+                   flowLayout(checkboxGroupInput("dataGroup", label = h3("Data for Model:"), 
+                                      choices = list("Cleveland Clinic (US)" = 1, "Hungary" = 2, "VA Long Beach (US)" = 3, "Switzerland"= 4),
+                                      selected = c(1)),
+                   checkboxGroupInput("painFilter", label = h3("Filter by Symptoms:"), 
+                                                                             choices = list("Include patients with pain" = 1, "Include patients without pain" = 2),
+                                                                             selected = c(1:2)),
+                   htmlOutput("summary",container = tags$div,class="well", style="background:#EFEFEF;width:100%;")),
+                                      h3("The Currently Selected Source Patients, Used to Train the Model:"),
+                                      plotOutput('plot1')
+                                      ),
+          tabPanel("Model", 
+                   h3("The Model: An \"rpart\" Classification Tree Created from Data Selection:"),
+                   plotOutput('plot2'),
+                   h3("Candidate patient values:"),
+                   tableOutput("values")
+                 ),
+          tabPanel("Validity", 
+                   
+                   h4("Background on Evaluation of Learning Models:"),
+                   p("It is important to evaluate machine learning models to show their effectiveness at predicting new events. Cross-validation is an internal sampling method for evaluating whether a model is successful making predictions on new data elements.  The statistics are provided below are 95% confidence intervals based on 500 randomly-seeded repeats of k-fold cross validations, where k=10. 
+                   In other words, a random 10% subset of the source selection is tested on a model trained on the other 90%, 5000 times."), 
+                   p("Poor results in cross validation are caused by imbalance of disease status in the source selection, an absence of reliable predictors,
+                   or a low overall number of individuals in data selected. As such, the models are generally weak for females, applying filtering by symptoms, and using the VA and Switzerland data. Counterintuitively, the absence of pain is very a strong predictor of disease in these data sets. This may indicate a sampling bias. Thus additional consideration of issues related to clinical validity is also required. In many cases, the primary factor for the model's predictive capacity is that the presence of pain appears to be a protective factor."),
+                   htmlOutput("validation",container = tags$div,class="well", style="background:#EFEFEF;width:80%;"),
+                   htmlOutput("valid", style="width:80%")
+                   )
+        )
+       )
+       
+        
+      ), theme = "bootstrap.css"
+   )
+ 
+
+
+# Defines server functions 
+server <- function(input, output) {
+  
+  
+# Reactive expression to compose a training data containing the values of appropriate gender
+  makeReactiveBinding("raw") 
+  filtered <- reactive({
+    input$sex
+    input$dataGroup
+    input$painFilter
+    isolate({
+      raw <- data.frame(ID = integer(),
+                        age = integer(), 
+                        sex = factor(),
+                        cp = factor(),
+                        trestbps = integer(),
+                        chol = integer(),
+                        fbs = factor(),
+                        restecg = factor(),
+                        thalach = integer(),
+                        exang = factor(),
+                        oldpeak = double(),
+                        slope = factor(),
+                        ca = factor(),
+                        thal = factor(),
+                        class = factor(),
+                        cv_disease = factor(),
+                        color_logic = integer(),
+                        line_logic = double(),
+                        mean.of.chol.bps = double(),
+                        stringsAsFactors=FALSE) 
+      if (1 %in% input$dataGroup){
+      raw <- rbind (raw, read.csv("cleveland_labels.csv",na.strings = "?")) 
+      }
+      if (2 %in% input$dataGroup){
+      raw <- rbind (raw, read.csv("hungarian_labels.csv",na.strings = "?"))
+      }
+      if (3 %in% input$dataGroup){
+        raw <- rbind (raw, read.csv("va.csv",na.strings = "?"))
+      }
+      if (4 %in% input$dataGroup){
+        raw <- rbind (raw, read.csv("switzerland.csv",na.strings = "?"))
+      }
+      raw <- subset(raw, sex %in% input$sex)
+      
+      noPain <- subset(raw, cp %in% c("None"))
+      pain <- subset(raw, cp %in% c("1","2","3"))
+      
+      if ((1 %in% input$painFilter) && (2 %in% input$painFilter)){
+        raw <- rbind (noPain, pain)
+      }else if ((1 %in% input$painFilter) && !(2 %in% input$painFilter)){
+        raw <- pain
+      }else if (!(1 %in% input$painFilter) && (2 %in% input$painFilter)){
+        raw <- noPain
+      }
+      
+      })
+    }) 
+ 
+#Summary for the selected data set. 
+output$summary <-renderTable({
+  
+    frame <-count(filtered(),vars = c("cv_disease"))
+   names(frame)<-c("Source Breakdown","# of Patients")
+   return(frame)
+  
+})
+
+  
+#Reactive expression to compose of Test Patient object
+  test <- reactive({
+    # Compose data frame
+    data.frame(
+      age = input$age, 
+      sex = input$sex,
+      cp = input$cp,
+      trestbps = input$trestbps,
+      chol = input$chol,
+      fbs = input$fbs,
+      restecg = input$restecg,
+      mean.of.chol.bps = (input$chol + input$trestbps)/2,  
+      stringsAsFactors=FALSE)
+  }) 
+  
+#Reactive expression to compose of Model object
+  rtree <- reactive({
+    tree <- rpart(cv_disease ~ trestbps + age + chol + fbs + restecg + cp, 
+      data = filtered(), control = rpart.control(cp = 0.02), method = "class", x= TRUE)
+    #tree$frame$yval <- as.numeric(rownames(tree$frame))
+    return(tree)
+  }) 
+  
+#Cross validation
+
+  kfolds <- reactive ({ 
+
+    F1.st <- c()
+    precision.st <- c()
+    recall.st <- c()
+    
+    k<-10
+    folds <- createFolds(filtered()$cv_disease, k=k, list=TRUE, returnTrain = TRUE)
+    for (i in 1:k) {
+      xtree <- rpart(
+        cv_disease ~  age + restecg + trestbps + chol + fbs + cp, 
+        data = filtered()[folds[[i]],], method = "class")
+      predictions <- predict(object = xtree, newdata = filtered()[-folds[[i]],], type = "class")
+      F1.st<- c(F1.st,confusionMatrix(predictions, filtered()[-folds[[i]], ]$cv_disease,positive = ">50% Blockage")$byClass[c("F1")])
+      precision.st <- c(precision.st,confusionMatrix(predictions, filtered()[-folds[[i]], ]$cv_disease,positive = ">50% Blockage")$byClass[c("Precision")])
+      recall.st <- c(recall.st,confusionMatrix(predictions, filtered()[-folds[[i]], ]$cv_disease,positive = ">50% Blockage")$byClass[c("Recall")])
+    }  
+    v <- data.frame(F1.st,precision.st,recall.st)
+    print(v)
+    return(v) 
+    
+})
+  
+    # Output the validation statistics
+    output$validation <- renderText({
+      temp <- kstats()
+    paste("<h3>Internal Model Validity  based on Source Selections:</h3><br>The <span style=\"font-weight:bold\">F1 score</span>, which is a weighted indicator of overall accuracy, is<span style=\"font-weight:bold\">",format(round(temp["F1"],2), nsmall = 2),"±",format(round((1.96*temp["SDF1"]),2), nsmall = 2),"</span><br>",
+    "The <span style=\"font-weight:bold\">Recall </span>(Sens.), the proportion of true blockages correctly predicted, is<span style=\"font-weight:bold\">",format(round(temp["Recall"],2), nsmall = 2),"±",format(round((1.96*temp["SDR"]),2), nsmall = 2),"</span><br>",
+    "The <span style=\"font-weight:bold\">Precision </span>(PPV), the proportion a blockage predictions that are true, is<span style=\"font-weight:bold\">",format(round(temp["Precision"],2), nsmall = 2),"±",format(round((1.96*temp["SDP"]),2), nsmall = 2),"</span><br>",
+    "<p>*95% Confidence Intervals from repeated k-fold cross validations where n=500 and k=10<p>")
+    })
+  
+  output$valid <- renderText({
+     text1 <- "<span style=\"font-size:150%;font-weight:bold\">Model shows an internal predictive validity.</span>"
+     text2 <- ""
+     temp <- kstats()
+     temprecall <- temp["Recall"]
+     tempsd <- temp["SDR"]
+    if (temprecall > 0.94) {
+      text1 <- ""
+      text2 <- paste(text2,"<span style=\"color:red;font-size:150%;font-weight:bold\">🚑 Warning: High Recall.</span><br> Model created from very high prevalence source data and inadequate for predicting healthy patients.<br>")
+    } 
+     if (temprecall < 0.04) {
+       text1 <- ""
+       text2 <- paste(text2,"<span style=\"color:red;font-size:150%;font-weight:bold\">🚑 Warning: No Recall.</span><br> Model created from very low prevalence source data and is inadequate for predictions of diseased patients.<br>")
+     } 
+     if(temprecall < 0.55) {
+      text1 <- ""
+      text2 <- paste(text2,"<span style=\"color:red;font-size:150%;font-weight:bold\">📊 Warning: Low Recall.</span><br> In cross-validation, the model preforms at around random chance or worse. This may be caused by a large imbalance of disease and no disease, and low numbers in the source selection, which may create imbalanced cross validation folds, that models the complete source poorly.<br>")
+    } 
+     if(tempsd > 0.20) {
+      text1 <- ""
+      text2 <- paste(text2,"<span style=\"color:red;font-size:150%;font-weight:bold\">📊 Warning: High Variation in Cross-Validation.</span><br> Use with caution. May not be enough similar individuals to preform cross validation without imbalanced folds.<br>")
+     }
+     return(paste(text1,text2))
+     
+  })
+  
+  kstats <- reactive ({
+    x <- data.frame(F1=double(),Precision = double,Recall = double())
+    y <- data.frame(F1=double(),Precision = double,Recall = double())
+    F1.st <- c()
+    precision.st <- c()
+    recall.st <- c()
+    
+    x<-replicate(500,kfolds())
+    
+     for (j in 1 : 500) { 
+      y <- rbind(y,x[,j])
+    }
+    data.frame(F1 = mean(y$F1.st,na.rm = TRUE),
+               Precision = mean(y$precision.st,na.rm = TRUE),
+               Recall = mean(y$recall.st,na.rm = TRUE),
+               SDF1 = sd(y$F1.st,na.rm = TRUE),
+               SDP = sd(y$precision.st,na.rm = TRUE),
+               SDR = sd(y$recall.st,na.rm = TRUE),  
+               stringsAsFactors=FALSE) 
+  })
+  
+#   stats <- reactive ({
+#   
+#     data = data.frame(F1 = 0.8,
+#     Precision = .85,
+#     Recall = .75,
+#     SDF1 = 0.1,
+#     SDP = 0.1,
+#     SDR = 0.1,  
+#     stringsAsFactors=FALSE
+#     ) 
+#     return(data)
+# })
+  
+# Primary model function: create the prediction and output the answer.
+  output$prediction <- renderText({
+   
+    if (test()$cp == "" || test()$fbs == "" ||test()$restecg == "" ){return(paste("<div style=\"color:red;font-size:115%\">Enter all 6 pretest attributes for the candidate patient to make a prediction.</div>"))}
+   
+    pred <- data.frame(predict(rtree(), test(), type = "prob"))
+    #print(pred)
+    pred <- format(round(pred[1,1], 2), nsmall = 2)
+    if (pred <= 0.2) {
+    pred <- toString(pred)
+    pred <- paste("<span style=\"color:green;font-size:150%;font-weight: bold;\">💚 Very low likelihood of blockage.</span><br>The recursive partition model predicts only",pred,"probability of a coronary artery blockage of greater than 50%.", sep = " ")
+    return(pred)
+    }else if ((pred < 0.5) && (pred > 0.2)) {
+      pred <- toString(pred)
+      pred <- paste("<span style=\"color:orange;font-size:150%;font-weight: bold;\">💛 Low likelihood of a blockage.</span><br>The recursive partition model predicts only",pred,"probability of a coronary artery blockage of greater than 50%.", sep = " ")
+      return(pred)
+    }else if ((pred >= 0.5) && (pred < 0.8)) {
+      pred <- toString(pred)
+      pred <- paste("<span style=\"color:red;font-size:150%;font-weight: bold;\">❤️ Elevated probability of a blockage.</span><br>The recursive partition model predicts ",pred,"probability of a coronary artery blockage of greater than 50%.", sep = " ")
+      return(pred)
+    }else if (pred >= 0.8) {
+      pred <- toString(pred)
+      pred <- paste("<span style=\"color:darkred;font-size:150%;font-weight: bold;\">🖤 High probability of a blockage.</span><br>The recursive partition model predicts ",pred,"probability of a coronary artery blockage of greater than 50%.", sep = " ")
+      return(pred)
+    }else {return("Additional patient information needed for prediction")}
+    })
+  
+# Show the test patient values using an HTML table
+ output$values <- renderTable({
+    drops<-names(test()) %in% c("mean.of.chol.bps")
+    test()[!drops]
+   })
+  
+# Define data for the plot
+  selectedData <- reactive({
+    filtered()[c("age","mean.of.chol.bps")]
+  })
+  
+  color <- reactive({
+    filtered()[c("color_logic","line_logic")]
+  })
+  
+# Create the plot of the reference data
+  output$plot1 <- renderPlot({
+    #colors
+    palette(c("#e1e1e1","#DE7480","#555555", "#888888"))
+    #margins
+    par(mar = c(5.1, 4.1, 1, 1))
+    #training points
+    plot(selectedData(),
+         lwd = 0.5,
+         bg = color()$color_logic,
+         col = "#222222",
+         pch = 21,
+         cex = 1.9,
+         cex.axis = 1.2,
+         cex.lab = 1.4,
+         xlab = "Age",
+         ylab = "Average of BP and Cholesterol")
+    #test points
+    points(test()[c("age","mean.of.chol.bps")], col = "#428BCA", bg = "white", pch = 21, cex = 2.2, lwd = 9)
+    legend("topright", legend = c("(-)cath","(+)cath","candidate"), pch = c(21,21,21), col = c("black","black", "#428BCA"),
+           cex = 1, pt.cex = 2 , pt.bg=c("#e1e1e1","#DE7480","white"), pt.lwd= c(0.5,0.5,8) ,ncol=2)
+  })
+# Create the plot of the reference data
+  output$plot2 <- renderPlot({
+    rpart.plot(rtree(), type = 4, extra = 108, xflip = TRUE, compress=TRUE, split.col="black", split.cex = 1.2, branch.col = "darkgray", under.col= "darkgray", leaf.round=0, round=0, box.palette = "RdYlGn")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
